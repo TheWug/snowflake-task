ba1c40ebe90ca8bb23485e29ccd9b8ac2267ba01
Ryan S <wuuug@telegram>
2021-05-20 03:07:25 -0700

    Create a security group, allowing HTTP+SSH and all outbound

--- main.go
+++ main.go
@@ -34,6 +34,40 @@ func main() {
 			return err
 		}
 
+		// create a new security group, which permits inbound on tcp ports 22 and 80, and any outbound
+		sec_group, err := ec2.NewSecurityGroup(ctx, "public-http-server", &ec2.SecurityGroupArgs{
+			Name:        pulumi.String("basic-http-ssh"),
+			Description: pulumi.String("Allow incoming HTTP and SSH traffic, and any outgoing traffic."),
+			Ingress:     ec2.SecurityGroupIngressArray{
+				ec2.SecurityGroupIngressArgs{
+					CidrBlocks:  pS2A(pulumi.String("0.0.0.0/0")),
+					FromPort:    pulumi.Int(80),
+					ToPort:      pulumi.Int(80),
+					Protocol:    pulumi.String("tcp"),
+					Description: pulumi.String("Allow inbound simple HTTP"),
+				},
+				ec2.SecurityGroupIngressArgs{
+					CidrBlocks:  pS2A(pulumi.String("0.0.0.0/0")),
+					FromPort:    pulumi.Int(22),
+					ToPort:      pulumi.Int(22),
+					Protocol:    pulumi.String("tcp"),
+					Description: pulumi.String("Allow inbound SSH"),
+				},
+			},
+			Egress:      ec2.SecurityGroupEgressArray{
+				ec2.SecurityGroupEgressArgs{
+					CidrBlocks:  pS2A(pulumi.String("0.0.0.0/0")),
+					FromPort:    pulumi.Int((0)),
+					ToPort:      pulumi.Int(0),
+					Protocol:    pulumi.String("-1"),
+					Description: pulumi.String("Allow all outbound traffic"),
+				},
+			},
+		})
+		if err != nil {
+			return err
+		}
+
 		// create a keypair from your provided public key
 		keypair, err := ec2.NewKeyPair(ctx, "instance-sshkey", &ec2.KeyPairArgs{
 			PublicKey:     pulumi.String(string(pubkey)),
@@ -47,6 +81,9 @@ func main() {
 		instance, err := ec2.NewInstance(ctx, "web-server-basic-demo", &ec2.InstanceArgs{
 			Ami:                 pulumi.String("ami-0d382e80be7ffdae5"), // ubuntu 20.04 x86-64
 			InstanceType:        pulumi.String(ec2.InstanceType_T2_Micro),
+			VpcSecurityGroupIds: pulumi.StringArray{
+				sec_group.ID(),
+			},
 			UserData:            pulumi.String(string(script)),
 			KeyName:             keypair.KeyName,
 		})
