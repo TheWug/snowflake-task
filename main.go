deb352e3978d92ee334856e81dc8fedbf0f5fab9
Ryan S <wuuug@telegram>
2021-05-20 03:21:15 -0700

    Add configuration option for public key file for convenience
    key is patched into instance so you can ssh in with a key you
    already have instead of needing to generate and install a new oner

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"github.com/pulumi/pulumi-aws/sdk/v4/go/aws/ec2"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
+
+	"io/ioutil"
 )
 
 func pS2A(str pulumi.String) pulumi.StringArray {
@@ -13,13 +16,34 @@ func pS2A(str pulumi.String) pulumi.StringArray {
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		args := &ec2.InstanceArgs{
-			Ami: pulumi.String("ami-0d382e80be7ffdae5"), // ubuntu 20.04 x86-64
-			InstanceType: pulumi.String(ec2.InstanceType_T2_Micro),
+
+		/*
+		required configuration options:
+		deploy-script (you can set it to deploy_webserver.sh)
+		public-key-file (you can set it to your favorite RSA public key)
+		*/
+
+		conf := config.New(ctx, "")
+		pubkey, err := ioutil.ReadFile(conf.Require("public-key-file"))
+		if err != nil {
+			return err
 		}
 
-		// Create an AWS resource (S3 Bucket)
-		instance, err := ec2.NewInstance(ctx, "web-server-basic-hello", args)
+		// create a keypair from your provided public key
+		keypair, err := ec2.NewKeyPair(ctx, "instance-sshkey", &ec2.KeyPairArgs{
+			PublicKey:     pulumi.String(string(pubkey)),
+			KeyNamePrefix: pulumi.String("instance-sshkey"),
+		})
+		if err != nil {
+			return err
+		}
+
+		// create an ec2 instance with the provided public key and feed it the deployment script
+		instance, err := ec2.NewInstance(ctx, "web-server-basic-demo", &ec2.InstanceArgs{
+			Ami:                 pulumi.String("ami-0d382e80be7ffdae5"), // ubuntu 20.04 x86-64
+			InstanceType:        pulumi.String(ec2.InstanceType_T2_Micro),
+			KeyName:             keypair.KeyName,
+		})
 		if err != nil {
 			return err
 		}
